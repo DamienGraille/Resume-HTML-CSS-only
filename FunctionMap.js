215783720c8053ae11cdef83af5636c2279aab7c
Damien1989 <damiengraille@gmail.com>
2019-02-12 23:04:49 +0000

    Simplify JS map part of the resume
    Create and organise a list. Which data type to save all the values for
    each town : location(lat, long),marker(title), window(string)?

---
+++ FunctionMap.js
@@ -0,0 +1,8 @@
+<script>
+    const London= [
+        {locLondon : {lat: 51.507351, lng: -0.12775}},
+        {markerLondon : {title: London}},
+        {londonwindow:'<p>London, UK</p>'}
+    ]
+    console.log(London);
+</script>
