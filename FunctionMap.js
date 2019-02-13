4216486961ee6c32936ad9130fdc3759af26462c
Damien1989 <damiengraille@gmail.com>
2019-02-13 22:53:21 +0000

    Add function for Google map API
    Add function for Google map API.
    Function takes array of locations with lat and lgn. Loop goes through
    array and add marker as well title on each marker

--- FunctionMap.js
+++
@@ -1,8 +0,0 @@
-<script>
-    const London= [
-        {locLondon : {lat: 51.507351, lng: -0.12775}},
-        {markerLondon : {title: London}},
-        {londonwindow:'<p>London, UK</p>'}
-    ]
-    console.log(London);
-</script>
